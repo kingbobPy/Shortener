76219adc13b0a6108b77feaabbf810eafa181592
KingBob <78462047+kingbobPy@users.noreply.github.com>
2021-02-04 15:40:43 +0700

    Update shortener.py

--- shortener.py
+++ shortener.py
@@ -1,12 +1,12 @@
-#made by kingbob
+#made by whyenvy
 #credit me
 #!/usr/bin/python3
 import urllib.request
 
 
 #mui
-print("[+] TinyURL Link Shortener by kingbobPy")
-print("[+] Github : https://github.com/kingbobPy")
+print("[+] TinyURL Link Shortener by whyenvy")
+print("[+] Github : https://github.com/whyenvy")
 print("[+] Usage : python3 shortener.py")
 linkurl = input("[>] URL > ")
 print('\n')
